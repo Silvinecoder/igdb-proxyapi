bfba4c48ef63d8a773a6e5ff7d54a64825d65368
Cristiana-Simoes <cristiana.simoes@versantus.co.uk>
2022-01-10 14:05:11 +0000

    tweaking the ReadMe

--- api.js
+++ api.js
@@ -1,34 +1,24 @@
 import axios from 'axios'
 
 
-async function loadGames (ID, search = '') { //async due to await, search is to search games url is ?search=Mario, if the search is undefined is empty
+async function loadGames (ID, search = '') { 
 
-    const { API_URL, API_TOKEN } = process.env // this is the same as below - destructuring assignment - 
-    // const URL = process.env.URL
-    // const TOKEN = process.env.TOKEN
+    const { API_URL, API_TOKEN } = process.env 
 
     if (search)
-        search = `?search=${search}` // if search has information inside we want it to be ?search= whatever info is inside
+        search = `?search=${search}`
 
     if (ID)
-        ID = `?id=${ID}` // if id has information inside we want it to be ?search= whatever info is inside
+        ID = `?id=${ID}`
     
+    if (!API_URL || !API_TOKEN )  console.error('missing info') 
 
-    // if (!URL || !TOKEN )  throw new Error('missing info') -  if its empty or Falsy add an error, throw new Error terminates the process
-    if (!API_URL || !API_TOKEN )  console.error('missing info') // is less destructive than throw new error 
-
-    const response = await axios(API_URL + search + ID, { // default is get if you want post you do axios.post
+    const response = await axios(API_URL + search + ID, { 
     headers: {
-        Authorization: `Bearer ${API_TOKEN}` // auth of the token to show data 
+        Authorization: `Bearer ${API_TOKEN}`
     }}) 
 
-
-
-    return response.data // to show data of the api
+    return response.data 
 }
 
-export { loadGames } // export async function loadgames
-
-// env means environment variables - by env it won't be in our code 
-// git ignore for envirnoment and node_modules so it doesn't send to git 
-// download yarn add dotenv
+export { loadGames }
--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 import express from 'express'
-import dotenv from 'dotenv' // env variables are secure because they are not tracked by git 
+import dotenv from 'dotenv'
 
-dotenv.config() // load everything in env, to process.env
+dotenv.config() 
 
 const app = express()
 const port = process.env.PORT || 3000 
 
-import { loadGames } from './api.js' // the . is the directory that you are in or it will find it in the node_modules 
+import { loadGames } from './api.js'
 
-//MiddleWare
+// MiddleWare
  
 function checkAuth (req, res, next){
-    if (req.headers.authorization?.split(' ')[1] !== process.env.TOKEN) // if the request is different than the token 
-    //we are taking the token and split with a space because it's bearer : token and we chose the first in the array cos bearer is one and token is another due to not being joined together 
-        return res.status(401).send('Unauthorized') // return 401 if it's not unauthorized 
-
-    // There's two different API one is Nuno's the Api we are requesting and the other is the one that we've made 
-    next()
+    if (req.headers.authorization?.split(' ')[1] !== process.env.TOKEN) 
      
+        return res.status(401).send('Unauthorized') 
+
+    next()  
 }
 
 app.get('/', (req, res) => {
     res.send( 'this is home')
 })
 
-app.get('/games', checkAuth, async (req, res) => { // it has to be in async 
-    const { id, search } = req.query // to request the search that is in the query - if they don't include the ?search it's undefined, if it's undefined we are still sending it to loadGames
+app.get('/games', checkAuth, async (req, res) => { 
+    const { id, search } = req.query 
     res.json(await loadGames(id, search))   
 })
 
